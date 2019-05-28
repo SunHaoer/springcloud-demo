357c734c232ec437412cdf22826dfff3c8685e11
wuxiujun <lyqf365@outlook.com>
2019-05-28 14:26:38 +0800

    init

---
+++ demo/service1/src/main/java/pro/sunhao/service1/Service1Application.java
@@ -0,0 +1,24 @@
+package pro.sunhao.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class Service1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service1Application.class, args);
+    }
+
+    @RequestMapping("/service1")
+    public String service1(){
+        return "service1";
+    }
+
+}
